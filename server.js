45bf26ee89f15f61c0575068825e921e6f4567a9
mingchan <nick.ccm1122@gmail.com>
2016-06-16 00:39:01 +0800

    mod

--- server.js
+++ server.js
@@ -3,21 +3,6 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 5000;
-
 app.use(morgan('dev'));
-// make express look in the dist directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index.html');
-});
-
-app.listen(port, function() {
-    // console.log('Our app is running on http://localhost:' + port);
-});
+app.use(gzippo.staticGzip(__dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
